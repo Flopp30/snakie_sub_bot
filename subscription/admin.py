ad9c16425754619e8f26d37983939f241aa5879e
Artem Pospeev <pospeev.artem@icloud.com>
2023-12-06 10:55:46 +0300

    added user registration date

--- subscription/admin.py
+++ subscription/admin.py
@@ -10,11 +10,19 @@ from subscription.models import Subscription
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'is_active', 'is_auto_renew', 'first_sub_', 'start_date', 'unsub_date', 'user_',
+        'id', 'is_active', 'is_auto_renew', 'first_sub_', 'first_coming_', 'start_date', 'unsub_date', 'user_',
         'product',
         'payment_amount', 'payment_currency'
     )
     list_filter = (
+        (
+            "user__registration_datetime",
+            DateRangeQuickSelectListFilterBuilder(
+                title="Дата регистрации пользователя",
+                default_start=datetime.now(),
+                default_end=datetime.now(),
+            ),
+        ),
         (
             "user__first_sub_date",
             DateRangeQuickSelectListFilterBuilder(
@@ -62,3 +70,9 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
     first_sub_.short_description = 'Дата первой подписки'
     first_sub_.admin_order_field = 'user__first_sub_date'
+
+    def first_coming_(self, obj):
+        return obj.user.registration_datetime
+
+    first_coming_.short_description = 'Дата регистрации пользователя'
+    first_coming_.admin_order_field = 'user__registration_datetime'
